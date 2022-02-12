ec86e934898a5a99dc7a2024ac8db2d45be181d1
夏宁 <478084629@qq.com>
2022-02-12 22:52:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>hello APP 2022/2/12 test
+        <div>
+            <img src="@/assets/1.png" alt="">
+        </div>
+        <router-view />
+    </div>
+</template>
+<script>
+export default {
+    name:'',
+    mounted(){
+        console.log('1');
+        console.log('at');
+        console.log(process.env.NODE_ENV);
+    }
+}
+</script>
+<style lang="scss" scoped>
+ div{
+     color: red;
+     font-size: 27px;
+     background: #f6f6f6;
+     transform: translate(100px);
+     backface-visibility: hidden;
+ }
+</style>
